c464a7220d78cf767f3d08b942829bdeda40e09e
Paul Bienkowski <pb@opatut.de>
2017-10-28 01:31:03 +0200

    Implement store init

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ fn main() {
         ("git", Some(matches)) =>    { app.git_exec(vcs, &matches); true }
         ("rm", Some(matches)) =>     { app.remove(vcs, &matches); true }
         ("grep", Some(matches)) =>   { app.grep(&matches); true }
+        ("init", Some(matches)) =>   { app.init(&matches); true }
         _ => false
     };
 
@@ -238,6 +239,15 @@ impl PassstoreApp {
             println!("Error: {} is not in the password store.", pass);
         }
     }
+
+    fn init(&mut self, matches: &ArgMatches) {
+        let gpgid = matches.value_of("GPGID").unwrap_or("");
+
+        match self.store.init(gpgid) {
+            Ok(_) => (),
+            Err(err) => panic!("{}", err)
+        }
+    }
 }
 
 
@@ -346,6 +356,13 @@ fn get_matches<'a>() -> ArgMatches<'a> {
                     .arg(Arg::with_name("PARAMS")
                          .multiple(true)
                          .required(true)))
+        .subcommand(SubCommand::with_name("init")
+                    .about("Initialize new password storage and use gpg-id for encryption.")
+                    .arg(Arg::with_name("GPGID")
+                         .help("identifier for gpg key to use for encryption, can \
+                               be either of key id/fingerprint, or user id")
+                         .required(true)
+                         .index(1)))
         .get_matches()
 }
 
